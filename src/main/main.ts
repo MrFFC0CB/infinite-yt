eb9f72600516a371ba23fa3deff8f7d0c0efda7e
MrFFC0CB <190535678+MrFFC0CB@users.noreply.github.com>
2025-12-11 20:48:29 -0300

    Add express server.

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,17 +1,32 @@
-const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const express = require('express');
+
+const runServer = () => {
+	let port = 1312;
+	const server = express();
+
+	// server.use(express.static(path.join(__dirname, 'public')));
+	server.use(express.static('build'));
+
+	server.listen(port, () => {
+		console.log(`Server running on port ${port}`);
+	});
+};
 
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 1024,
 		height: 768,
-		icon: './public/images/favicon.ico',
+		icon: './assets/images/favicon.ico',
 		// autoHideMenuBar: true,
 	});
 
-	win.loadFile(path.join(__dirname, 'index.html'));
+	// win.loadFile(path.join(__dirname, 'index.html'));
+	win.loadURL('http://localhost:1312');
 };
 
 app.whenReady().then(() => {
+	runServer();
 	createWindow();
 });
