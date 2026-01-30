b8c975ddd3f4cc171738cc339310663ce7642adb
MrFFC0CB <190535678+MrFFC0CB@users.noreply.github.com>
2026-01-30 17:55:43 -0300

    Fix loading logic on SearchResults

--- src/ui/pages/SearchResults.tsx
+++ src/ui/pages/SearchResults.tsx
@@ -5,51 +5,46 @@ import IconLoading from "../components/icons/IconLoading";
 
 export default function SearchResults() {
 	const keyword: string = useParams().query || '';
-	const [ loading, setLoading ] = useState<boolean>(false);
-	const [ searchResults, setSearchResults ] = useState<VideoDataType[]>([]);
+	const [ searchResults, setSearchResults ] = useState<VideoDataType[] | null>(null);
 	const { setTitle } = useOutletContext<LayoutContext>();
 
 	useEffect(() => {
 		if (!keyword) return;
-
-		setLoading(true);
 
 		window.api.fetchSearchResults(keyword)
 			.then(results => {
 				setSearchResults(results);
 			})
 			.finally(() => {
-				setLoading(false);
 				setTitle(`Results for: ${keyword}`);
 			});
 	}, [keyword]);
 
-	if (loading) {
-		return (
-			<div style={{position: "absolute", inset: "0", display: "grid", placeItems: "center"}}>
-				<p style={{fontSize: "1.5rem", textAlign: "center", lineHeight: "1.2"}}>
-					Searching for:
-					<br/>
-					<strong style={{color: "#b88be1"}}>{keyword}</strong>
-					<br/><br/>
-					<IconLoading />
-				</p>
-			</div>
-		)
-	}
-	if (searchResults.length === 0) {
-		return (
-			<div style={{position: "absolute", inset: "0", display: "grid", placeItems: "center"}}>
-				<p style={{textAlign: "center"}}>No results for: {keyword}</p>
-			</div>
-		)
+	if (searchResults) {
+		if (searchResults.length === 0) {
+			return (
+				<div style={{position: "absolute", inset: "0", display: "grid", placeItems: "center"}}>
+					<p style={{textAlign: "center"}}>No results for: {keyword}</p>
+				</div>
+			)
+		}
+
+		if (searchResults.length > 0) {
+			return (
+				<ListVideos videos={searchResults} />
+			);
+		}
 	}
 
 	return (
-		<>
-			{searchResults.length > 0 &&
-				<ListVideos videos={searchResults} />
-			}
-		</>
-	);
+		<div style={{position: "absolute", inset: "0", display: "grid", placeItems: "center"}}>
+			<p style={{fontSize: "1.5rem", textAlign: "center", lineHeight: "1.2"}}>
+				Searching for:
+				<br/>
+				<strong style={{color: "#b88be1"}}>{keyword}</strong>
+				<br/><br/>
+				<IconLoading />
+			</p>
+		</div>
+	)
 };
